084bde2a62dc193e3ed8a23555886d3f6fd64646
Bhuyan, Shouvik (US - Bengaluru) <shbhuyan@deloitte.com>
2017-04-11 15:39:28 +0530

    init

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-var express = require('express');
-var app = express();
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
 
-app.set('port', (process.env.PORT || 5000));
+// Get our API routes
+const api = require('./server/routes/api');
 
-app.use(express.static(__dirname + '/public'));
+const app = express();
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Set our api routes
+app.use('/api', api);
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '5000';
+app.set('port', port);
 
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
